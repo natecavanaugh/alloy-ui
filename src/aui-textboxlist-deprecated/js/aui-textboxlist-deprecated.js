a5416e9d90aabee66525e61b4e28a5301f66192a
Jonathan Mak <jonathan.mak@liferay.com>
2013-10-27 19:52:23 -0700

    AUI-1028 - Source formatting

--- src/aui-textboxlist-deprecated/js/aui-textboxlist-deprecated.js
+++ src/aui-textboxlist-deprecated/js/aui-textboxlist-deprecated.js
@@ -13,6 +13,7 @@ var Lang = A.Lang,
 
 	BOUNDING_BOX = 'boundingBox',
 	CONTENT_BOX = 'contentBox',
+	ESCAPED_CONTENT = 'escapedContent',
 
 	CONFIG_ANIM = {
 		from: {
@@ -433,7 +434,7 @@ var TextboxList = A.Component.create(
 				var eventType = event.type;
 				var inputNode = instance.inputNode;
 				var entryHolder = instance.entryHolder;
-				var escaped = instance.get('escapedContent');
+				var escaped = instance.get(ESCAPED_CONTENT);
 				var item = event.item;
 				var index = event.index;
 
@@ -445,8 +446,8 @@ var TextboxList = A.Component.create(
 					if (eventType == 'dataset:add') {
 						var entry = new TextboxListEntry(
 							{
-								labelText: key,
-								escapedContent: escaped
+								escapedContent: escaped,
+								labelText: key
 							}
 						);
 
@@ -494,14 +495,14 @@ var TextboxListEntry = A.Component.create(
 		NAME: ENTRY_NAME,
 
 		ATTRS: {
+			escapedContent: {
+				value: false
+			},
 			labelText: {
 				value: ''
 			},
 			tabIndex: {
 				value: 0
-			},
-			escapedContent: {
-				value: false
 			}
 		},
 
@@ -512,17 +513,15 @@ var TextboxListEntry = A.Component.create(
 				var instance = this;
 
 				var contentBox = instance.get(CONTENT_BOX);
+				var escapedContent = instance.get(ESCAPED_CONTENT);
+				var labelText = instance.get('labelText');
 
 				var text = A.Node.create(TPL_ENTRY_TEXT);
 				var close = A.Node.create(TPL_ENTRY_REMOVE);
 
-				var labelText = A.Escape.html(instance.get('labelText'));
+				var labelTextVal = escapedContent ? A.Escape.html(labelText) : labelText;
 
-				if (instance.get('escapedContent')) {
-					labelText = instance.get('labelText');
-				}
-
-				text.set('innerHTML', labelText);
+				text.set('innerHTML', labelTextVal);
 
 				contentBox.appendChild(text);
 				contentBox.appendChild(close);
